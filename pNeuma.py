7fe913f303e3755ae32898175d25f9b113cb9c0a
Yunus Emre Avcı <yunusemravci@users.noreply.github.com>
2020-06-08 14:01:25 +0300

    filters added

--- pNeuma.py
+++ pNeuma.py
@@ -7,6 +7,7 @@ Created on Thu May 14 22:53:54 2020
 import pandas as pd
 import numpy as np
 import os
+import csv
 import scipy.io as sio
 
 # =============================================================================
@@ -15,36 +16,60 @@ import scipy.io as sio
 
 class pNeuma:
     
-    path = os.getcwd() + '/pneuma_sample_dataset/pneuma_sample_dataset.csv'
+    path = os.getcwd() + '/pneuma_sample_dataset/pneuma_piece.csv'
     
+    df=pd.DataFrame()
     
     def load(self):
-        df=pd.read_csv(self.path, header = None)
-        #df = pd.read_csv(self.path)
-        print(df.shape)
-        print(df.columns.tolist())
-        return df
+
+        headers = pd.read_csv(self.path, nrows=0).columns.tolist()
+        word = headers[0]
+
+        splitted = word.split(';')
+        #print(splitted[0])
+        self.df = pd.read_csv(self.path,delimiter=';')
+
+        #print(df[splitted[1]])
+        return splitted
 # =============================================================================
 # saves the dataframe into matlab    
 # =============================================================================
-    def savetomatlab(self,df):
+    def savetomatlab(self):
 #        truncDataFrame.rename(columns=lambda x:'col_' + x.replace(' ', '_'), inplace=True)  
 #        scipy.io.savemat('EEGdata2.mat', {'struct2':truncDataFrame.to_dict("list")})
-        sio.savemat('pneuma_sample_dataset.mat', df)
+        sio.savemat('pneuma_sample_dataset.mat', self.df)
         
 # =============================================================================
 # finds and returns count of vehicle type
 # =============================================================================
-    def vehicle_type_number(self):
+    def vehicle_type_number(self,vehicle_type,dff):
         
+        counter = 0        
+        allvehicle_types = self.df[dff[1]]
+
+        for i in range(len(allvehicle_types)):
+            
+            vehicle = allvehicle_types[i]
+            if vehicle.replace(' ', '') == vehicle_type:
+                counter += 1
         
-        return 
+        return counter
 # =============================================================================
-#  returns all data of specified vehicle type e.g. car   
+# returns all data of specified vehicle type e.g. car 
+#         
 # =============================================================================
     def vehicle_type_data(self,vehicle_type):
         
-        return
+        typed_data = pd.DataFrame()
+        
+        for i in range(len(self.df)):
+           allvehicle_types = self.df[dff[1]]  
+           vehicle = allvehicle_types[i]
+           if vehicle.replace(' ', '') == vehicle_type: 
+               typed_data = typed_data.append(self.df.iloc[i, :])
+        
+        print(typed_data)
+        return typed_data
 
 # =============================================================================
 # counts the number of vehicle in specified area which coord specified by 
@@ -64,7 +89,7 @@ class pNeuma:
 # =============================================================================
 pn = pNeuma()
 dff = pn.load()
-
-print(dff.take([1],axis=1).head())
+print(pn.vehicle_type_number("Car",dff))
+pn.vehicle_type_data("Motorcycle")
 
 
